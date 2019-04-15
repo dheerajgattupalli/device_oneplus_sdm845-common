57acfd672d6347363532f16f56021f4a1e529d13
Jiyong Park <jiyong@google.com>
2019-04-15 14:19:03 +0900

    sdm845-common: Add updatable_apex.mk
    The product abstracts configs that need to be set to support updating of APEXes.
    
    Change-Id: Id5aa944ceba6a6846308f1c18a08af8b3bb38589

--- common.mk
+++ common.mk
@@ -20,6 +20,9 @@ $(call inherit-product, vendor/oneplus/sdm845-common/sdm845-common-vendor.mk)
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
